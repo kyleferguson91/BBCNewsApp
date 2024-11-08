6e5f1980416f9c3361d17ac995a40a4e7fd69aeb
Kyle Ferguson <kyleferguson91@gmail.com>
2024-11-08 12:43:37 -0500

    fragment selection working

--- app/src/main/java/com/example/androidlab/MainActivity.java
+++ app/src/main/java/com/example/androidlab/MainActivity.java
@@ -15,6 +15,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -66,7 +67,39 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected( MenuItem item) {
         String itemTitle = item.getTitle().toString();
         Log.d("MainActivity", "Selected item: " + itemTitle);
+        getSupportActionBar().setTitle(" " + itemTitle);
+
+     //
+        if(itemTitle.equals("News Feed".trim()))
+        {
+            System.out.println("load news feed frag");
+            loadFragment(new NewsFeedFragment());
+        }
+        if(itemTitle.equals("Settings".trim()))
+        {
+            System.out.println("load settings frag");
+            loadFragment(new settings());
+        }
+        if(itemTitle.equals("Profile".trim()))
+        {
+            System.out.println("load profile frag");
+            loadFragment(new profileFragment());
+        }
+        if(itemTitle.equals("Favorites".trim()))
+        {
+            System.out.println("load favs frag");
+            loadFragment(new favsFragment());
+        }
+
+
         return super.onOptionsItemSelected(item);
     }
 
+    // Helper method to handle fragment transactions
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
